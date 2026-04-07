1673c813c546ee0534f338de8bde6f9f17c99ed3
samiyaabdullah786 <samiyaabdullah789@gmail.com>
2026-04-07 19:54:43 +0500

    complete portfolio

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
+const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const Navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    Navbar.classList.toggle('active');
+});
+
+const activePage = () => {
+    const barsBox = document.querySelector('.bars-box');
+    const header = document.querySelector('header');
+
+     header.classList.remove('active');
+    setTimeout(() => {
+        header.classList.add('active');
+    },1100);
+
+    navLinks.forEach(link => {
+        link.classList.remove('active')
+    });
+
+    barsBox.classList.remove('active');
+    setTimeout(() => {
+        barsBox.classList.add('active');
+    },1100);
+
+    sections.forEach(section => {
+        section.classList.remove('active')
+    });
+     menuIcon.classList.remove('bx-x');
+    Navbar.classList.remove('active');
+    
+}
+
+navLinks.forEach((link, idx) => {
+    link.addEventListener('click', () => {
+        if(!link.classList.contains('active')) {
+            activePage();
+
+            link.classList.add('active');
+
+            setTimeout(() => {
+                sections[idx].classList.add ('active');
+            }, 1100);
+        }
+    });
+});
+
+logoLink.addEventListener('click', () => {
+    if(!navLinks[0].classList.contains('active')) {
+        activePage();
+
+        navLinks[0].classList.add('active');
+
+        setTimeout(() => {
+                sections[0].classList.add ('active');
+            }, 1100);
+    }
+});
+
 const resumeBtns = document.querySelectorAll('.resume-btn');
 
 resumeBtns.forEach((btn, idx) => {
@@ -14,4 +77,47 @@ resumeBtns.forEach((btn, idx) => {
         });
         resumeDetails[idx].classList.add('active');
     });
+});
+
+const arrowRight = document.querySelector ('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector ('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+    const portfolioDetails = document.querySelectorAll('.portfolio-detail')
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+
+    portfolioDetails.forEach(detail => {
+        detail.classList.remove('active');
+    });
+    portfolioDetails[index].classList.add('active')
+}
+
+arrowRight.addEventListener('click',() => {
+    if(index < 4) {
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }
+    else{
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click',() => {
+    if(index > 1) {
+        index--;
+        arrowRight.classList.remove('disabled');
+    }
+    else{
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activePortfolio();
 });
